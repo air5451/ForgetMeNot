94e71747d3885dcbd4ea4bfcf36f261235293d9f
km2549 <km2549>
2020-07-29 09:55:29 -0700

    basic phone, sms and email integration

--- src/App/Views/OrganizationPage.xaml.cs
+++ src/App/Views/OrganizationPage.xaml.cs
@@ -12,10 +12,18 @@ namespace ForgetMeNot.App.Views
         public OrganizationPage(string organizationId)
         {
             InitializeComponent();
-            BindingContext = new OrganizationViewModel(organizationId);
+
+            OrganizationViewModel vvmOrg = new OrganizationViewModel(organizationId);
+            BindingContext = vvmOrg;
+
+            Organization.Address addressData = vvmOrg.Organization.Addr;
+            Organization.Hours hoursData = vvmOrg.Organization.Hrs;
+
+            this.AddressControl.Text = $"{addressData.Address1} {addressData.Address2} {addressData.City} {addressData.State}";
+            this.HoursControl.Text = $"M {hoursData.Monday}, T {hoursData.Tuesday}, W {hoursData.Wednesday}, T {hoursData.Thursday}, F {hoursData.Friday}, S {hoursData.Saturday}, S {hoursData.Sunday}";
         }
 
-        private async void Button_Clicked(object sender, EventArgs e)
+        private async void Directions_Clicked(object sender, EventArgs e)
         {
             Organization organization = ((OrganizationViewModel)BindingContext).Organization;
             Organization.Address address = organization.Addr;
@@ -26,16 +34,32 @@ namespace ForgetMeNot.App.Views
                 Thoroughfare = address.Address1 + " " + address.Address2,
                 Locality = address.City
             };
-            var options = new MapLaunchOptions { Name = organization.Name };
 
-            try
+            var options = new MapLaunchOptions { Name = organization.Name };
+            await Map.OpenAsync(placemark, options);
+        }
+
+        private async void Email_Clicked(object sender, EventArgs e)
+        {
+            var message = new EmailMessage
             {
-                await Map.OpenAsync(placemark, options);
-            }
-            catch (Exception ex)
-            {
-                // No map application available to open or placemark can not be located
-            }
+                Subject = "",
+                Body = "",
+                To = new List<string> { ((OrganizationViewModel)BindingContext).Organization.Email }
+            };
+
+            await Email.ComposeAsync(message);
+        }
+
+        private void Phone_Clicked(object sender, EventArgs e)
+        {
+            PhoneDialer.Open(((OrganizationViewModel)BindingContext).Organization.Phone);
+        }
+
+        private async void Sms_Clicked(object sender, EventArgs e)
+        {
+            var message = new SmsMessage("", new[] { ((OrganizationViewModel)BindingContext).Organization.Phone });
+            await Sms.ComposeAsync(message);
         }
     }
 }
