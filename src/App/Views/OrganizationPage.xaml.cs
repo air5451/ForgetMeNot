1b60d82f4758354350a7aefe0995b1e716b118c5
km2549 <km2549>
2020-07-28 22:06:55 -0700

    added button to launch maps

--- src/App/Views/OrganizationPage.xaml.cs
+++ src/App/Views/OrganizationPage.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using ForgetMeNot.App.Models;
 using ForgetMeNot.App.ViewModels;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace ForgetMeNot.App.Views
@@ -12,6 +13,29 @@ namespace ForgetMeNot.App.Views
         {
             InitializeComponent();
             BindingContext = new OrganizationViewModel(organizationId);
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            Organization organization = ((OrganizationViewModel)BindingContext).Organization;
+            Organization.Address address = organization.Addr;
+            var placemark = new Placemark
+            {
+                CountryName = address.Country,
+                AdminArea = address.State,
+                Thoroughfare = address.Address1 + " " + address.Address2,
+                Locality = address.City
+            };
+            var options = new MapLaunchOptions { Name = organization.Name };
+
+            try
+            {
+                await Map.OpenAsync(placemark, options);
+            }
+            catch (Exception ex)
+            {
+                // No map application available to open or placemark can not be located
+            }
         }
     }
 }
